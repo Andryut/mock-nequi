986cf62faa6b1015dfb5ab8e2e02b57567afa8d3
Andryut <aahuertasca@unal.edu.co>
2018-11-28 14:27:38 -0500

    Menu node builder is completed

--- menu_node_builder.rb
+++ menu_node_builder.rb
@@ -1,10 +1,23 @@
 class MenuNodeBuilder
 
-  
+  def initialize
+    @view_options = Array.new
+    @node_options = Array.new
+    @back_option = [:text => "exit", :node => nil]
+  end
+
+  def with_back_option text:, node:
+    @back_option = [:text => text, :node => node]
+  end
+
+  def add_option text:, node:
+    @view_options << text
+    @node_options << node
+  end
 
   def build
-    #view =
-    #controller =
+    view = MenuView.new options: @view_options, back_option: @back_option[:text]
+    controller = MenuController.new option_nodes: @node_options, back_option: @back_option[:node]
     return NavigationNode.new view: view, controller: controller
   end
 end
