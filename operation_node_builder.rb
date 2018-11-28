0a4c8071a4a7e7dabb5dfd23f2e43810d3431de2
Andryut <aahuertasca@unal.edu.co>
2018-11-28 14:55:23 -0500

    Operatio node builder completed

--- operation_node_builder.rb
+++ operation_node_builder.rb
@@ -1,3 +1,29 @@
 class OperationNodeBuilder
 
+  def initialize
+    @input_views = Array.new
+  end
+
+  def add_input view:
+    @input_views << view
+  end
+
+  def with_validation proc:
+    @validation_proc = proc
+  end
+
+  def with_action proc:
+    @action_proc = proc
+  end
+
+  def with_back node:
+    @back_node = node
+  end
+
+  def buid
+    view = OperationView.new input_views: @input_views
+    controller = OperationController.new validation_proc: @validation_proc, action_proc: @action_proc
+    controller.back_node = @back_node
+    return NavigationNode.new view: view, controller: controller
+  end
 end
