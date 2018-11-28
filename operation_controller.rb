9f1aa2be410da3f2d3d9cf17f92372cbcd2261c5
Andryut <aahuertasca@unal.edu.co>
2018-11-28 14:57:04 -0500

    Operatio node correction

--- operation_controller.rb
+++ operation_controller.rb
@@ -2,13 +2,13 @@ class OperationController
 
   attr_writer :back_node
 
-  def initialize validate_proc:, action_proc:
-    @validate = validate_proc
+  def initialize validation_proc:, action_proc:
+    @validation = validation_proc
     @action = action_proc
   end
 
   def execute inputed_data:
-    if @validate.call inputed_data then
+    if @validation.call inputed_data then
       @action.call inputed_data
     end
     @back_node.play
