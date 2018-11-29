33bd6ef0c7b501c7cdfbc8f8f745694392da2f4a
Andryut <aahuertasca@unal.edu.co>
2018-11-29 11:51:01 -0500

    Solving a problem in the operation_controller, that leave us pass data that was previously configured to the action proc.

--- operation_controller.rb
+++ operation_controller.rb
@@ -1,16 +1,15 @@
 class OperationController
 
-  attr_writer :back_node
+  attr_writer :back_node, :configured_data
 
-  def initialize validation_proc:, action_proc:, session:
+  def initialize validation_proc:, action_proc:
     @validation = validation_proc
     @action = action_proc
-    @session = session
   end
 
   def execute inputed_data:
-    if @validation.call inputed_data, session then
-      @action.call inputed_data, session
+    if @validation.call inputed_data, @configured_data then
+      @action.call inputed_data, @configured_data
     end
     @back_node.play
   end
