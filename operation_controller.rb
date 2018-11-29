038355a6b2d1d6a506070cfaef65d5e752a75f81
Andryut <aahuertasca@unal.edu.co>
2018-11-28 21:26:37 -0500

    Adding the session object that represents the model to the operation controller

--- operation_controller.rb
+++ operation_controller.rb
@@ -2,14 +2,15 @@ class OperationController
 
   attr_writer :back_node
 
-  def initialize validation_proc:, action_proc:
+  def initialize validation_proc:, action_proc:, session:
     @validation = validation_proc
     @action = action_proc
+    @session = session
   end
 
   def execute inputed_data:
-    if @validation.call inputed_data then
-      @action.call inputed_data
+    if @validation.call inputed_data, session then
+      @action.call inputed_data, session
     end
     @back_node.play
   end
