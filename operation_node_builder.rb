91cf3becc11316e261a53f203d829a8e2686faae
Andryut <aahuertasca@unal.edu.co>
2018-11-29 11:59:51 -0500

    Adapted operation_node_builder

--- operation_node_builder.rb
+++ operation_node_builder.rb
@@ -1,13 +1,17 @@
 class OperationNodeBuilder
 
-  def initialize session:
-    @session = session
+  def initialize
+    @configured_data = Hash.new
     @input_views = Array.new
     @back_node = nil
   end
 
   def add_input view:
     @input_views << view
+  end
+
+  def add_configured_data key:, value:
+    @configured_data.merge!({key => value})
   end
 
   def with_validation proc:
@@ -24,7 +28,8 @@ class OperationNodeBuilder
 
   def buid
     view = OperationView.new input_views: @input_views
-    controller = OperationController.new validation_proc: @validation_proc, action_proc: @action_proc, session: @session
+    controller = OperationController.new validation_proc: @validation_proc, action_proc: @action_proc
+    controller.configured_data = @configured_data
     controller.back_node = @back_node
     return NavigationNode.new view: view, controller: controller
   end
