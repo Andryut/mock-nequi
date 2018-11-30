8bb96477bc8c854751e48c6145e6375c58b5357b
Andryut <aahuertasca@unal.edu.co>
2018-11-30 14:35:20 -0500

    Operation_view correction in the name of the variables.

--- operation_view.rb
+++ operation_view.rb
@@ -1,13 +1,13 @@
 class OperationView
 
-  def initialize input_views:
-    @input_views = input_views
+  def initialize input_nodes:
+    @input_nodes = input_nodes
   end
 
   def show
     input_data = Hash.new
-    input_views.each do |input_view|
-      input_data.merge!(input_view.play)
+    input_nodes.each do |input_node|
+      input_data.merge!(input_node.play)
     end
     return input_data
   end
