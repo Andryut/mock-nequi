92c116acb37f648a0c0d2d63debf550583474229
Andryut <aahuertasca@unal.edu.co>
2018-11-30 12:40:44 -0500

    Planning the main logic

---
+++ bank_system.rb
@@ -0,0 +1,18 @@
+class BankSystem
+
+  def initialize
+    self.configure_model
+    self.configure_navigation_tree
+  end
+
+  def start
+  end
+
+  protected
+
+  def configure_navigation_tree
+  end
+
+  def configure_model
+  end
+end
