a3f9e2f97fbd2c7eec50fa5c8e7ffec0a250b7d0
Jhonatan Torres <yon.t.f@hotmail.com>
2018-12-02 14:18:20 -0500

    user-login

--- test/model_test.rb
+++ test/model_test.rb
@@ -1,18 +1,24 @@
+puts 'conecting to database...'
 require File.join(__dir__, '../connection', 'connection.rb')
-
+puts 'loading packages...'
 require File.join(__dir__, '../model', 'user.rb')
 require File.join(__dir__, '../model', 'account.rb')
 require File.join(__dir__, '../model', 'general_account.rb')
 require File.join(__dir__, '../model', 'pocket_account.rb')
+require File.join(__dir__, '../model', 'movement.rb')
+require File.join(__dir__, '../model', 'transfer_movement.rb')
+require File.join(__dir__, '../model', 'transaction_movement.rb')
+require File.join(__dir__, '../model', 'transaction_type.rb')
 require File.join(__dir__, '../model', 'transaction.rb')
-require File.join(__dir__, '../model', 'transfer_transaction.rb')
-require File.join(__dir__, '../model', 'operation_transaction.rb')
-require File.join(__dir__, '../model', 'operation_type.rb')
-require File.join(__dir__, '../model', 'operation.rb')
 require File.join(__dir__, '../model', 'transfer.rb')
 require File.join(__dir__, '../model', 'coffer.rb')
 require File.join(__dir__, '../model', 'mattress_coffer.rb')
 require File.join(__dir__, '../model', 'goal_coffer.rb')
 require File.join(__dir__, '../model', 'goal.rb')
+puts 'ready'
 
-puts GeneralAccount.first.owner.mattress.values
+begin 
+    puts User.login(email:'1', password:'pass').values
+rescue RuntimeError => e
+    puts e.message
+end
