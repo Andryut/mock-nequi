1917b2400d985e78bfe72151edee1b00c5b9cb9f
Andryut <aahuertasca@unal.edu.co>
2018-12-02 20:06:40 -0500

    play function added to the navigation tree

--- navigation_tree.rb
+++ navigation_tree.rb
@@ -1,9 +1,11 @@
 class NavigationTree
 
-  attr_reader :root
-
   def initialize model_objects:
     access_menu = AccessMenu.new model_objects: model_objects
     @root = access_menu.menu_node
+  end
+
+  def play
+    @root.play
   end
 end
