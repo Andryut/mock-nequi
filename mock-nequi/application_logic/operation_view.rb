29a7445f1d90250f0862d48185f2d82947a7a27b
Andryut <aahuertasca@unal.edu.co>
2018-12-03 16:06:37 -0500

    Clean screen added

--- mock-nequi/application_logic/operation_view.rb
+++ mock-nequi/application_logic/operation_view.rb
@@ -1,10 +1,11 @@
-class OperationView
+class OperationView < View
 
   def initialize input_views:
     @input_views = input_views
   end
 
   def show
+    clean_screen
     form = Hash.new
     @input_views.each do |input_view|
       form.merge!(input_view.show)
