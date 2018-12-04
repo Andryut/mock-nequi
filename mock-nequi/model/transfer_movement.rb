3e245cfd52e5216633ca93ab27067cb3d1817c44
Jhonatan Torres <yon.t.f@hotmail.com>
2018-12-04 17:29:54 -0500

    transfer report

--- mock-nequi/model/transfer_movement.rb
+++ mock-nequi/model/transfer_movement.rb
@@ -1,5 +1,5 @@
 class TransferMovement < Sequel::Model(DB[:movements].where(type: Movement.transfer_type))
   
-  one_to_one :transfer, class: :Transfer, key: :associated_movement
+  one_to_one :transfer, key: :associated_movement
   
 end
--- mock-nequi/view/transfer_report.rb
+++ mock-nequi/view/transfer_report.rb
@@ -12,7 +12,7 @@ class TransferReport < ReportView
         transfer_movements.each do |transfer_movement|
             break unless count < limit
             transfer = transfer_movement.transfer
-            receiver = transfer.eceiver
+            receiver = User[transfer.receiver]
             puts ' Date: ' + transfer_movement.date.to_s
             puts ' Amount: $%0.2f' % transfer_movement.amount_money
             puts ' Receiver: ' + receiver.email
